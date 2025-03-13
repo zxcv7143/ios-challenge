58d3bead0d9a5693453ccdfbd3753944113cdb65
Anton Zuev <zxcv7143@gmail.com>
2025-03-13 14:34:41 +0100

    Initial Commit

---
+++ AdList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdList
+//
+//  Created by Anton Zuev on 13/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
