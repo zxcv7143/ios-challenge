06c040a4300060ad2fef233aa90441ccb479a17d
Anton Zuev <zxcv7143@gmail.com>
2025-03-14 20:09:11 +0100

    feat: make the app load programmatically

--- App/SceneDelegate.swift
+++ App/SceneDelegate.swift
@@ -16,7 +16,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: scene)
+        
+        let navigationController = UINavigationController(rootViewController: AdListRouter.createAdListModule())
+        //navigationController.viewControllers = [AdListRouter.createAdListModule()]
+        window.rootViewController = navigationController
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -47,7 +54,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
 
         // Save changes in the application's managed object context when the application transitions to the background.
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+        PersistenceManager.shared.saveContext()
     }
 
 
