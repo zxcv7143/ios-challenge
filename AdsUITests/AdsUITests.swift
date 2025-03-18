8d72a58e5a402fb0bda08061679b2ec069d2eb74
Anton Zuev <zxcv7143@gmail.com>
2025-03-18 17:33:41 +0100

    feat: added Test Plan

--- AdsUITests/AdsUITests.swift
+++ AdsUITests/AdsUITests.swift
@@ -45,14 +45,4 @@ final class AdListUITests: XCTestCase {
         app.buttons["detailMapButton"].tap()
         XCTAssert(app.otherElements["mapView"].exists)
     }
-    
-    @MainActor
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
